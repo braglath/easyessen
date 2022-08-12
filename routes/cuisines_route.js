074d698bdb6f0da4f2bb48d6f86da5a4e33f7c95
bragu <87694668+braglath@users.noreply.github.com>
2022-08-12 18:56:01 +0530

    ft - cuisine filter - done

--- routes/cuisines_route.js
+++ routes/cuisines_route.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
-
-const cuisinesModel = require("../models/cuisines_model");
+const foodModel = require("../models/food_model");
 
 router.get("/", (req, res) => {
   res.json(cuisinesModel);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(` cuisine id - ${id}`);
+  var foodWithCuisine = foodModel.data.filter((e) => e.cuisineId == id);
+  res.json(foodWithCuisine);
 });
 
 module.exports = router;
