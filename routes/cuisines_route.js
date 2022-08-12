e3072d949b27dfa8f6b3137450d60d141bd93405
bragu <87694668+braglath@users.noreply.github.com>
2022-08-12 18:57:40 +0530

    bug - cuisines and console - done

--- routes/cuisines_route.js
+++ routes/cuisines_route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const foodModel = require("../models/food_model");
+const cuisinesModel = require("../models/cuisines_model");
 
 router.get("/", (req, res) => {
   res.json(cuisinesModel);
@@ -8,7 +9,7 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-  console.log(` cuisine id - ${id}`);
+  //   console.log(` cuisine id - ${id}`);
   var foodWithCuisine = foodModel.data.filter((e) => e.cuisineId == id);
   res.json(foodWithCuisine);
 });
