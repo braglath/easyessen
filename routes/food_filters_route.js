063c5e142947fee9447b9ac714a82222f938bb5f
bragu <87694668+braglath@users.noreply.github.com>
2022-08-12 18:47:57 +0530

    bug - removed console prints - done

--- routes/food_filters_route.js
+++ routes/food_filters_route.js
@@ -5,7 +5,6 @@ const foodModel = require("../models/food_model");
 
 router.route("/offers").get((req, res) => {
   var foodWithOffers = foodModel.data.filter((e) => e.offers.length !== 0);
-  console.log(`foodWithOffers length - ${foodWithOffers.length}`);
   res.json({
     data: foodWithOffers,
   });
@@ -13,7 +12,6 @@ router.route("/offers").get((req, res) => {
 
 router.route("/veg").get((req, res) => {
   var foodIsVeg = foodModel.data.filter((e) => e.isVeg === true);
-  console.log(`foodWithOffers length - ${foodIsVeg.length}`);
   res.json({
     data: foodIsVeg,
   });
