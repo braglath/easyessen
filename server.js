57f0e13f84328b38cf0f6995fb499291cc603d7d
bragu <87694668+braglath@users.noreply.github.com>
2022-08-12 16:55:25 +0530

    added error handler

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 
 const foodRoute = require("./routes/food_route");
+const errorHandler = require("./middlewares/error_handler");
 
 app.use(express.json());
 
 app.use("/", foodRoute);
+app.use(errorHandler);
 
 app.listen(5000, () => {
-  console.log("- Sever listening on http://localhost:5000");
+  console.log(
+    "- Sever listening on http://localhost:5000 or http://13.126.10.179:5000"
+  );
 });
