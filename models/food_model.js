cff93ec648d46cbb66bca9a3f9b66dac4047666e
bragu <87694668+braglath@users.noreply.github.com>
2022-08-12 16:48:45 +0530

    edited food model

--- models/food_model.js
+++ models/food_model.js
@@ -1,10 +1,11 @@
 module.exports = {
   data: [
     {
+      id: 1,
       image:
         "https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000",
       name: "Chicken Wings Barbecue",
-      foodType: "North Indian,Chinese",
+      foodType: "North Indian, Chinese",
       rating: 3.5,
       isVeg: false,
       offers: [
@@ -15,16 +16,186 @@ module.exports = {
       ],
     },
     {
+      id: 2,
       image:
-        "https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000",
-      name: "Chicken Wings Barbecue",
-      foodType: "North Indian,Chinese",
+        "https://img.freepik.com/premium-photo/big-hamburger-with-double-beef-french-fries_252907-8.jpg?w=2000",
+      name: "Cheese Burger with Finger Chips",
+      foodType: "Fast Food, USA",
       rating: 4.5,
       isVeg: false,
       offers: [
         {
-          percentage: 45,
-          couponeCode: "ASE45",
+          percentage: 15,
+          couponeCode: "USA15",
+        },
+      ],
+    },
+    {
+      id: 3,
+      image:
+        "https://www.thespruceeats.com/thmb/yLyKgqIj6-SLt9gJkgkjfIZtfC0=/4300x2867/filters:no_upscale():max_bytes(150000):strip_icc()/chole-chickpea-curry-1957946-hero-01-cddf957cc78d4032ba29ec2adbc05a97.jpg",
+      name: "Fried Roti with Paneer Butter Masala",
+      foodType: "North Indian, Continental",
+      rating: 4.8,
+      isVeg: true,
+      offers: [
+        {
+          percentage: 55,
+          couponeCode: "EASY55",
+        },
+      ],
+    },
+    {
+      id: 4,
+      image:
+        "https://www.tasteofhome.com/wp-content/uploads/2021/01/tasty-butter-chicken-curry-dish-from-indian-cuisine-1277362334.jpg?fit=700,700",
+      name: "Spicy Chicken Curry",
+      foodType: "South Indian",
+      rating: 4.2,
+      isVeg: false,
+      offers: [],
+    },
+    {
+      id: 5,
+      image:
+        "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_30/2943911/190723-easy-chow-mein-al-1414.jpg",
+      name: "Chinese Noodles",
+      foodType: "Chinese Special",
+      rating: 3.5,
+      isVeg: true,
+      offers: [
+        {
+          percentage: 25,
+          couponeCode: "EASY25",
+        },
+      ],
+    },
+    {
+      id: 6,
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/5/59/A_traditional_indian_dish_of_bengal%28pulao-mangsha_with_misti_doi%29.jpg",
+      name: "South Indian Mini Meal",
+      foodType: "South Indian, Tradition",
+      rating: 4.1,
+      isVeg: true,
+      offers: [],
+    },
+    {
+      id: 7,
+      image:
+        "https://media.self.com/photos/57d8954324fe9dae32831913/master/pass/15-Classic-Dishes_Feature.png",
+      name: "Juicy Pasta",
+      foodType: "Fast Food, USA",
+      rating: 3.8,
+      isVeg: true,
+      offers: [
+        {
+          percentage: 25,
+          couponeCode: "EASY25",
+        },
+      ],
+    },
+    {
+      id: 8,
+      image: "https://i.ytimg.com/vi/ecLL7_voJRc/maxresdefault.jpg",
+      name: "Crispy Samosa",
+      foodType: "North Indian, Tradition",
+      rating: 3.8,
+      isVeg: true,
+      offers: [],
+    },
+    {
+      id: 9,
+      image:
+        "https://a.cdn-hotels.com/gdcs/production196/d970/cd40235b-5990-4067-8c05-c7d04711a312.jpg?impolicy=fcrop&w=800&h=533&q=medium",
+      name: "Juicy Chicken Leg",
+      foodType: "North Indian",
+      rating: 1.8,
+      isVeg: false,
+      offers: [
+        {
+          percentage: 8,
+          couponeCode: "EASY8",
+        },
+      ],
+    },
+    {
+      id: 10,
+      image:
+        "https://assets.traveltriangle.com/blog/wp-content/uploads/2018/02/Khandvi.jpg",
+      name: "Gujarati Veg Roll",
+      foodType: "North Indian, Traditional",
+      rating: 4.1,
+      isVeg: true,
+      offers: [],
+    },
+    {
+      id: 11,
+      image:
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/potluck-dishes-vegetarian-chili-macaroni-1574870284.jpg?crop=0.913xw:0.609xh;0,0.187xh&resize=640:*",
+      name: "Hot Spicy Pasta",
+      foodType: "Chinese",
+      rating: 2.8,
+      isVeg: true,
+      offers: [
+        {
+          percentage: 25,
+          couponeCode: "EASY25",
+        },
+      ],
+    },
+    {
+      id: 12,
+      image:
+        "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2021%2F05%2F17%2Fgarlic-butter-prime-rib-FT-RECIPE0621.jpg&q=60",
+      name: "Soft Beef Curry",
+      foodType: "Street Food, USA",
+      rating: 3.5,
+      isVeg: false,
+      offers: [
+        {
+          percentage: 15,
+          couponeCode: "EASY25",
+        },
+      ],
+    },
+    {
+      id: 13,
+      image:
+        "https://img.theculturetrip.com/450x/smart/wp-content/uploads/2017/11/1024px-chicken_65_dish.jpg",
+      name: "Prawn Pakoda",
+      foodType: "South Indian Special",
+      rating: 3.8,
+      isVeg: false,
+      offers: [
+        {
+          percentage: 15,
+          couponeCode: "EASY25",
+        },
+      ],
+    },
+    {
+      id: 14,
+      image:
+        "https://st2.depositphotos.com/3935465/7268/i/600/depositphotos_72687501-stock-photo-chickpeas-falafel-with-tzatziki-sauce.jpg",
+      name: "Egyptian Egg Balls",
+      foodType: "Fried Special",
+      rating: 3.5,
+      isVeg: false,
+      offers: [],
+    },
+    {
+      id: 15,
+      image:
+        "https://www.tasteofhome.com/wp-content/uploads/2021/01/view-of-the-kati-roll-companys-dish-kosha-mangsho-861455066.jpg?fit=700,700",
+      name: "Veg Frankie",
+      foodType: "North Indian, Sides",
+      rating: 3.5,
+      isVeg: true,
+      offers: [
+        {
+          percentage: 5,
+          couponeCode: "EASY5",
         },
       ],
     },
